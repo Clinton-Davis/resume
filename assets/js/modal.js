b72d686d13cd7c85480c042f206cf1565dafbcfc
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-10-19 17:54:03 +0100

    added new logo to mobile and desktop need to fix responsiveness

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -18,10 +18,10 @@ span.onclick = function () {
 }
 
 function startAnimation() {
-    const tl = gsap.timeline({ defaults: { ease: "bounce.out" } });
-    tl.fromTo(".progress-bar-fill-html", { width: "0%" }, { width: "92%", duration: 1 });
-    tl.fromTo(".progress-bar-fill-css", { width: "0%" }, { width: "87%", duration: 1 });
-    tl.fromTo(".progress-bar-fill-js", { width: "0%" }, { width: "75%", duration: 1 }, "+=.1");
-    tl.fromTo(".progress-bar-fill-py", { width: "0%" }, { width: "70%", duration: 1 }, "+=.2");
-    tl.fromTo(".progress-bar-fill-s ", { width: "0%" }, { width: "84%", duration: 1 }, "+=.3");
+    const tl = gsap.timeline()
+    tl.fromTo(".progress-bar-fill-html", { width: "0%" }, { width: "92%", duration: 1 })
+        .fromTo(".progress-bar-fill-css", { width: "0%" }, { width: "87%", duration: 2 }, "-=1")
+        .fromTo(".progress-bar-fill-js", { width: "0%" }, { width: "75%", duration: 2 }, "-=1.1")
+        .fromTo(".progress-bar-fill-py", { width: "0%" }, { width: "70%", duration: 2 }, "-=1.2")
+        .fromTo(".progress-bar-fill-s ", { width: "0%" }, { width: "84%", duration: 2 }, "-=1.3");
 }
