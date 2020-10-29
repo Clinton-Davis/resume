5059edf5e19ff8781f4b56a66f235401342f2d40
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2020-10-29 16:41:45 +0000

    enableded animations back to pages

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,44 +1,44 @@
-// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// tl.fromTo(
-//   ".big",
-//   { opacity: 0 },
-//   { opacity: 1, duration: 0.5, rotation: 360, stagger: 0.15 },
-//   "+=2"
-// );
-// tl.fromTo(
-//   ".left",
-//   { x: "-100000%" },
-//   { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
-// );
-// tl.fromTo(
-//   ".right",
-//   { x: "100000%" },
-//   { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
-// );
-// tl.fromTo(
-//   ".left2",
-//   { x: "-100000%" },
-//   { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
-// );
-// tl.fromTo(
-//   ".fade",
-//   { opacity: 0 },
-//   { opacity: 1, duration: 0.75, stagger: 0.1 }
-// );
+tl.fromTo(
+  ".big",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.5, rotation: 360, stagger: 0.15 },
+  "+=2"
+);
+tl.fromTo(
+  ".left",
+  { x: "-100000%" },
+  { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
+);
+tl.fromTo(
+  ".right",
+  { x: "100000%" },
+  { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
+);
+tl.fromTo(
+  ".left2",
+  { x: "-100000%" },
+  { x: "0%", duration: 0.5, rotation: 360, stagger: 0.05 }
+);
+tl.fromTo(
+  ".fade",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.75, stagger: 0.1 }
+);
 
-// tl.fromTo(".headinInfo", { opacity: "0" }, { opacity: "1", duration: 1 });
-// tl.fromTo("#top-nav", { y: "-1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
-// tl.fromTo(".img", { y: "-1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
+tl.fromTo(".headinInfo", { opacity: "0" }, { opacity: "1", duration: 1 });
+tl.fromTo("#top-nav", { y: "-1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
+tl.fromTo(".img", { y: "-1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
 
-// tl.fromTo("#btm-nav", 1.3, { y: "1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
-// tl.fromTo(".logo-name", { x: "-1000%" }, { x: "0%", duration: 1.3 }, "-=1.5");
-// tl.fromTo(".shadow5", { height: "0%" }, { height: "90%", duration: 1 }, "-=1");
-// tl.fromTo(".info", { opacity: "0" }, { opacity: "1", duration: 1.2 });
-// tl.fromTo(".nav-links", { x: "100%" }, { x: "0%", duration: 1 }, "-=2");
-// tl.fromTo(
-//   ".hero",
-//   { color: "#003d57" },
-//   { color: "#ffffff ", duration: 5 },
-//   "-=4"
-// );
+tl.fromTo("#btm-nav", 1.3, { y: "1000%" }, { y: "0%", duration: 1.3 }, "-=1.5");
+tl.fromTo(".logo-name", { x: "-1000%" }, { x: "0%", duration: 1.3 }, "-=1.5");
+tl.fromTo(".shadow5", { height: "0%" }, { height: "90%", duration: 1 }, "-=1");
+tl.fromTo(".info", { opacity: "0" }, { opacity: "1", duration: 1.2 });
+tl.fromTo(".nav-links", { x: "100%" }, { x: "0%", duration: 1 }, "-=2");
+tl.fromTo(
+  ".hero",
+  { color: "#003d57" },
+  { color: "#ffffff ", duration: 5 },
+  "-=4"
+);
